f4c4e2f44765075555a421f4921f1ffce1ac28aa
jannejaago <janneaps@gmail.com>
2019-11-05 16:06:43 +0200

    first commit

---
+++ Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Please write your name here:");
+            string name = ReadLine();
+            char[] characters = name.ToCharArray();
+            Array.Reverse(characters);
+            string reverseName = new string(characters);
+            bool checkIfPalindrome = name.Equals(reverseName, StringComparison.OrdinalIgnoreCase);
+            if (checkIfPalindrome == true)
+            {
+                WriteLine(name + " is a palindrome!");
+            }
+            else
+            {
+                WriteLine(name + " isn't a palindrome");
+            }
+            ReadLine();
+        }
+    }
+}
